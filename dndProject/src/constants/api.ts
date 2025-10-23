65e35ceb19adb6746f4f3196e961013bc2fbb491
Stameskaaa <chta02@mail.ru>
2025-10-23 23:24:00 +0500

    fix

--- dndProject/src/constants/api.ts
+++ dndProject/src/constants/api.ts
@@ -1,2 +1,2 @@
-export const baseUrl = import.meta.env.VITE_API_URL + '/management';
-export const clearUrl = import.meta.env.VITE_API_URL;
+export const baseUrl = (import.meta.env.VITE_API_URL || 'http://localhost/api') + '/management';
+export const clearUrl = import.meta.env.VITE_API_URL || 'http://localhost/api';
